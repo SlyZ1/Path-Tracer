b3710a81bcdd30481cd6cb50d8d06f6004e96931
Baptiste Girardin <baptiste.girardin@telecom-paris.fr>
2026-07-15 18:07:39 +0200

    +1

--- tools/gen_deps.ps1
+++ tools/gen_deps.ps1
@@ -8,8 +8,6 @@ param(
 $lines = Get-Content -LiteralPath $TmpFile -Encoding Default
 
 foreach ($line in $lines) {
-    # Ignore toute ligne /showIncludes "... : C:\...\fichier" (peu importe le préfixe/langue/extension,
-    # les headers standards comme <optional> n'ont pas d'extension)
     if ($line -notmatch ':\s*[A-Za-z]:\\') {
         Write-Host $line
     }
